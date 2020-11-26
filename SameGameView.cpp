3e859847ea8acc02d70bdf90003fdae45cfad5fd
yelskayakatya <yelskayakatya@gmail.com>
2020-11-26 12:07:29 +0100

    Добавьте файлы проекта.

---
+++ SameGameView.cpp
@@ -0,0 +1,105 @@
+﻿
+// SameGameView.cpp: реализация класса CSameGameView
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS можно определить в обработчиках фильтров просмотра реализации проекта ATL, эскизов
+// и поиска; позволяет совместно использовать код документа в данным проекте.
+#ifndef SHARED_HANDLERS
+#include "SameGame.h"
+#endif
+
+#include "SameGameDoc.h"
+#include "SameGameView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CSameGameView
+
+IMPLEMENT_DYNCREATE(CSameGameView, CView)
+
+BEGIN_MESSAGE_MAP(CSameGameView, CView)
+	// Стандартные команды печати
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// Создание или уничтожение CSameGameView
+
+CSameGameView::CSameGameView() noexcept
+{
+	// TODO: добавьте код создания
+
+}
+
+CSameGameView::~CSameGameView()
+{
+}
+
+BOOL CSameGameView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: изменить класс Window или стили посредством изменения
+	//  CREATESTRUCT cs
+
+	return CView::PreCreateWindow(cs);
+}
+
+// Рисование CSameGameView
+
+void CSameGameView::OnDraw(CDC* /*pDC*/)
+{
+	CSameGameDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: добавьте здесь код отрисовки для собственных данных
+}
+
+
+// Печать CSameGameView
+
+BOOL CSameGameView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// подготовка по умолчанию
+	return DoPreparePrinting(pInfo);
+}
+
+void CSameGameView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: добавьте дополнительную инициализацию перед печатью
+}
+
+void CSameGameView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: добавьте очистку после печати
+}
+
+
+// Диагностика CSameGameView
+
+#ifdef _DEBUG
+void CSameGameView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CSameGameView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CSameGameDoc* CSameGameView::GetDocument() const // встроена неотлаженная версия
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CSameGameDoc)));
+	return (CSameGameDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// Обработчики сообщений CSameGameView
